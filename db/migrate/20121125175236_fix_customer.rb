4dc1cb8b3155de32842a414d1e0a0539716f16ab
getabetterpic <dan@danielfsmith.com>
2012-11-25 18:04:57 +0000

    Added fix migration files.

---
+++ db/migrate/20121125175236_fix_customer.rb
@@ -0,0 +1,9 @@
+class FixCustomer < ActiveRecord::Migration
+  def up
+    change_column :customers, :phone_number, :string
+  end
+
+  def down
+    change_column :customers, :phone_number, :integer
+  end
+end
---
+++ db/migrate/20121125175247_fix_cust_contact.rb
@@ -0,0 +1,7 @@
+class FixCustContact < ActiveRecord::Migration
+  def up
+  end
+
+  def down
+  end
+end
